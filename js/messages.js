9ee51a4e06c2bb4977ecf39f3412207213d3682b
Thoriq Ibadurrohman <thoriq312@gmail.com>
2026-08-22 05:48:03 +0700

    perbaiki surat

--- js/messages.js
+++ js/messages.js
@@ -14,22 +14,27 @@
     {
       name: "Your kakak princess",
       photo: "images/radha.jpeg",
-      preview: "Welcome to 1/4 abad my adik!",
+      preview:
+        "Welcome to 1/4 abad my adik! Always be my cantik lucu gemes adik forever🩷",
       letter: [
         "Welcome to 1/4 abad my adik! Always be my cantik lucu gemes adik forever🩷",
-        "I hope your life is full of love and best best things. Happiness suits u well so please always be kind to yourself.",
-        "You deserve all good things in this world🩷",
+        "Lakuin apapun yang bikin bahagia ya. Happiness suits you well! Selalu ingat bahwa aida ga sendirian dan punya kakak yang selalu aida sebut princess ini yang akan selalu siap sedia (jadi grab apalagi).",
+        "Dari 25 tahun aida ada, kakak baru membersamai 4 tahun nih. Jadi ini pinky promise kita harus membersamai selama yang kita bisa yaaa.",
+        "I hope your life is full of love and best best things. PLEASE always be KIND to yourself ya.",
+        "You deserve all good things in this world🩷.",
+        "your princess, Radha",
       ],
     },
     {
-      name: "dea & namu sekeluarga",
+      name: "Dea & Namu Sekeluarga",
       photo: "images/dea.jpeg",
       preview:
         "Selamat ulang tahun my sweetest, smartest, kindest, partner ngopi, adik bungsu bebeb di kantor, Aida🥺💌",
       letter: [
         "Selamat ulang tahun my sweetest, smartest, kindest, partner ngopi, adik bungsu bebeb di kantor, Aida🥺💌",
         "Semoga my adik selalu sehat, panjang umur, dilimpahi kebahagiaan, dan terwujud semua cita-cita nya yaa!!!",
-        "Terimakasih selalu menemani dan selalu ada untuk kakak di setiap waktu!🥲 Tetap jadi Ai yang ceria dan peduli dengan sekitarnya, kurang2in beli PC tu plissss (dan yap) dan semoga segera dipertemukan dengan crush(es) di masa depan yaa! 😛 with love, dea & namu sekeluarga",
+        "Terimakasih selalu menemani dan selalu ada untuk kakak di setiap waktu!🥲 Tetap jadi Ai yang ceria dan peduli dengan sekitarnya, kurang2in beli PC tu plissss (dan yap) dan semoga segera dipertemukan dengan crush(es) di masa depan yaa! 😛",
+        "With love, Dea & Namu Sekeluarga",
       ],
     },
     {
@@ -38,16 +43,21 @@
       preview:
         "Dear Aida, our adik maknae 💝 Selamat memasuki quarter life era yaa..",
       letter: [
-        "Selamat memasuki quarter life era yaa.. plis tetap jadi Ai yang bahagia imut lucu dan ceria, selalu menemukan spark dalam hidup entah itu dengan lari, cokipop, new crush, ataupun teazzi tiap hari. Don’t be too hard on yourself, Ai yang sekarang udah sangat lebih dari cukup and I’m really proud of you! 🥹🥹 semoga hal-hal baik menghampiri ya Aiii",
+        "Dear Aida, our adik maknae 💝 Selamat memasuki quarter life era yaa..",
+        "Selamat memasuki quarter life era yaa.. plis tetap jadi Ai yang bahagia imut lucu dan ceria, selalu menemukan spark dalam hidup entah itu dengan lari, cokipop, new crush, ataupun teazzi tiap hari. Don’t be too hard on yourself, Ai yang sekarang udah sangat lebih dari cukup and I’m really proud of you! 🥹🥹",
+        "Semoga hal-hal baik menghampiri ya Aiii",
         "Lots of love, your number 1 Noona (Gadis Jember) 💝",
       ],
     },
     {
       name: "Pikrui",
       photo: "images/fikri.jpeg",
-      preview: "Happy birthday aidun",
+      preview: "Happy Birthday Aidun",
       letter: [
-        "Selamat memasuki umur quarter life crisis, meskipun kayanya sudah dimulai dulu dari tahun tahun sebelumnya ngehehe. Semoga sehat selalu dan panjang umur dan selalu jadi kebanggaan nongky mania dan orang di sekitarmu dengan pencapaian atau apapun yang kamu lakuin. Hope you found “the one” yh. semoga kamu bisa dapet yang selalu mendukung kamu dan ngga bingung. ",
+        "Happy birthday Aidun",
+        "Selamat memasuki umur quarter life crisis, meskipun kayanya sudah dimulai dulu dari tahun tahun sebelumnya ngehehe.",
+        "Semoga sehat selalu dan panjang umur dan selalu jadi kebanggaan nongky mania dan orang di sekitarmu dengan pencapaian atau apapun yang kamu lakuin.",
+        "Hope you found “the one” yh. semoga kamu bisa dapet yang selalu mendukung kamu dan ngga bingung. ",
         "You’re doing good.",
       ],
     },
@@ -57,9 +67,13 @@
       preview:
         "Selamat Ulang Tahun, Happy Birthday, Barakallah Fii Umrik, Otanjoubi Omedetou Aidaa",
       letter: [
+        "Selamat Ulang Tahun, Happy Birthday, Barakallah Fii Umrik, Otanjoubi Omedetou Aidaa",
         "You're a smart girl, healthy person, dan kamu punya tekad dan komitmen yang kuat.",
         "Aku harap kamu terus mempertahankan energy positif yang kamu punya dan sebarkan juga ke orang lain yaa.",
+        "Terima kasih atas perhatian, kepedulian, dan juga semua yang kamu lakukan untuk kami.",
+        "Semoga kamu selalu bahagia, sehat selalu dan semoga kamu bisa terus bertambah dewasa dan menjadi orang yang lebih baik lagi yaa.",
         "Tetap semangat, Aida! Sampai ketemu di nongky selanjutnya~",
+        "-your kakak bebeb king, Thoriq-",
       ],
     },
   ];
